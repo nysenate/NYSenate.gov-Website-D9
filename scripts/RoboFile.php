1b6d1c2abbf7d22d41b4cfc2cea18a539c749364
Joe Stewart <joe.stewart@mediacurrent.com>
2021-04-12 07:53:50 -0400

    MCIT-393 Add options to test:phpunit-test
    Change from test:php-unit-test.

--- scripts/RoboFile.php
+++ scripts/RoboFile.php
@@ -1,7 +1,7 @@
 <?php
 
-use  JoeStewart\RoboDrupalVM\Task\Vm;
-
+use JoeStewart\RoboDrupalVM\Task\Vm;
+use Symfony\Component\Console\Input\InputOption;
 class RoboFile extends \Robo\Tasks
 {
 
@@ -32,27 +32,44 @@ class RoboFile extends \Robo\Tasks
     }
 
     /**
-     * Run all PHPUnit unit tests.
+     * Run PHPUnit tests.
+     * @option $filter Optionally Filter which tests to run.
+     * @option $config_file Optionally specify the config file ( Defaults to "tests/phpunit/phpunit.xml").
+     * @option $test_dir Optionally specify the test directory ( Defaults to "web/modules/custom").
      */
-    public function testPhpUnitTests()
-    {
-        $this->stopOnFail(true);
-        $this->testPhpUnitCustomModulesTests();
-    }
+    public function testPhpunitTests(
+      $opts = [
+        'config_file' => InputOption::VALUE_REQUIRED,
+        'test_dir' => InputOption::VALUE_REQUIRED,
+        'filter' => InputOption::VALUE_REQUIRED,
+      ]
+    ) {
 
-    /**
-     * Run PHPUnit Unit tests on custom modules.
-     */
-    public function testPhpUnitCustomModulesTests()
-    {
-        $this->taskPhpUnit($this->_getProjectRoot() . '/vendor/bin/phpunit')
-            ->option('disallow-test-output')
-            ->option('strict-coverage')
-            ->option('-v')
-            ->option('-d error_reporting=-1')
-            ->configFile($this->_getProjectRoot() . '/tests/phpunit/phpunit.xml')
-            ->arg($this->_getProjectRoot() . '/web//modules/custom')
-            ->run();
+      $config_file = 'tests/phpunit/phpunit.xml';
+      if (!empty($opts['config_file'])) {
+        $config_file = $opts['config_file'];
+      }
+
+      $test_dir = 'web/modules/custom';
+      if (!empty($opts['test_dir'])) {
+        $test_dir = $opts['test_dir'];
+      }
+
+      if (empty($opts['filter'])) {
+        $opts['filter'] = 'Unit';
+      }
+
+      $this->stopOnFail(true);
+      $phpunit = $this->_getProjectRoot() . '/vendor/bin/phpunit';
+      $this->taskPhpUnit($phpunit)
+        ->option('disallow-test-output')
+        ->option('strict-coverage')
+        ->option('-v')
+        ->option('-d error_reporting=-1')
+        ->configFile($this->_getProjectRoot() . '/' . $config_file)
+        ->arg($this->_getProjectRoot() . '/' . $test_dir)
+        ->filter($opts['filter'])
+        ->run();
     }
 
     /**
@@ -64,7 +81,7 @@ class RoboFile extends \Robo\Tasks
     public function watchCustomModules()
     {
         $this->taskWatch()->monitor($this->_getProjectRoot() . '/web//modules/custom', function () {
-            $this->testPhpUnitCustomModulesTests();
+            $this->ttestPhpUnitTests();
         })->run();
     }
 

153c8b85c2bb647731f906c34e17976bf6fbdac1
Catherine Parkinson <catherine.parkinson@FA0159.home>
2022-02-04 17:21:15 -0600

    NYSD9-20: Default nysenate theme setup
    Theme custom/nysenate forked from rain_theme, config updated to set nysenate as default

---
+++ web/themes/custom/nysenate/src/patterns/components/event-carousel/event-carousel.es6.js
@@ -0,0 +1,54 @@
+/**
+ * @file
+ * Behaviors for the Filter Accordion.
+ */
+/* eslint-disable max-len */
+
+!((document, Drupal, $) => {
+  'use strict';
+
+  /**
+   * Setup and attach the Carousel behaviors.
+   *
+   * @type {Drupal~behavior}
+   */
+  Drupal.behaviors.eventCarousel = {
+    attach: function (context) {
+      // We want to only have the slick carousel trigger once or we will
+      // have a lot of pagers being appended on ajax trigger.
+      $('.event-carousel', context).once('eventCarousel').each(function () {
+        const $parentContainer = $(this);
+        const $list = $('.event-carousel__list', $parentContainer);
+        let $nav = $(this).closest('.event-carousel').find('.slick-pager');
+        $list.slick({
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          arrows: true,
+          fade: false,
+          appendArrows: $nav,
+          infinite: false,
+          responsive: [
+            {
+              // Not sure why the width being 1024 does not trigger this break
+              // point, when showing a 1024 wide desktop screen.
+              breakpoint: 1025,
+              settings: {
+                slidesToShow: 2,
+              },
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 1,
+              },
+            },
+          ],
+        });
+        $list.on('afterChange', function (event, slick) {
+          const isLastSlide = slick.$nextArrow.hasClass('slick-disabled');
+          $parentContainer.toggleClass('fade', !isLastSlide);
+        });
+      });
+    }
+  };
+})(document, Drupal, jQuery);

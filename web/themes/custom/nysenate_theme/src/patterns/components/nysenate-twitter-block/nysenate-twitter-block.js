e8adecfbe7578ab517641e0de550648582131ae2
Catherine Parkinson <catherine.parkinson@mediacurrent.com>
2022-04-28 17:14:29 -0500

    NYSD9-23: Global styles: typography and colors
    Editing global style variables, some SCSS linting

--- web/themes/custom/nysenate_theme/src/patterns/components/nysenate-twitter-block/nysenate-twitter-block.js
+++ web/themes/custom/nysenate_theme/src/patterns/components/nysenate-twitter-block/nysenate-twitter-block.js
@@ -10,6 +10,7 @@
         $iframeHead = $('iframe#twitter-widget-0').contents().find('head');
 
         if( !$('#twitter-widget-styles', $iframeHead).length ) { //If our stylesheet does not exist tey to place it
+          $iframeHead.append('<link rel="stylesheet" href="/themes/custom/nysenate_theme/dist/css/global.css" id="twitter-widget-styles">');
           $iframeHead.append('<link rel="stylesheet" href="/themes/custom/nysenate_theme/dist/css/nysenate-twitter-block.css" id="twitter-widget-styles">');
         }
         else if( $('#twitter-widget-styles', $iframeHead).length ) {    //If stylesheet exists then quit timer

7cce69ab78f8a6ae5a85cfb44cb5fe51ab1c1203
Patrick Ace Buen <patrick.ace.buen@codeandtheory.com>
2022-04-28 14:44:45 +0800

    NYSD9-48: Migrate Majority Issues taxonomy

--- web/modules/custom/nys_migrate/src/Plugin/migrate/source/FileEntity.php
+++ web/modules/custom/nys_migrate/src/Plugin/migrate/source/FileEntity.php
@@ -27,6 +27,31 @@ class FileEntity extends FieldableEntity {
     if (isset($this->configuration['type'])) {
       $query->condition('f.filemime', $query->escapeLike($this->configuration['type']) . "%", 'LIKE');
     }
+
+    // Get the alt text, if configured.
+    if (isset($this->configuration['get_alt'])) {
+      $alt_alias = $query->addJoin('left', 'field_data_field_file_image_alt_text', 'alt', 'f.fid = %alias.entity_id');
+      $query->addField($alt_alias, 'field_file_image_alt_text_value', 'alt');
+    }
+
+    // Get the title text, if configured.
+    if (isset($this->configuration['get_title'])) {
+      $title_alias = $query->addJoin('left', 'field_data_field_file_image_title_text', 'title', 'f.fid = %alias.entity_id');
+      $query->addField($title_alias, 'field_file_image_title_text_value', 'title');
+    }
+
+    // Get the width.
+    if (isset($this->configuration['get_width'])) {
+      $width_alias = $query->addJoin('left', 'field_data_field_image_main', 'width', 'f.fid = %alias.field_image_main_fid');
+      $query->addField($width_alias, 'field_image_main_width', 'width');
+    }
+
+    // Get the height.
+    if (isset($this->configuration['get_height'])) {
+      $height_alias = $query->addJoin('left', 'field_data_field_image_main', 'height', 'f.fid = %alias.field_image_main_fid');
+      $query->addField($height_alias, 'field_image_main_height', 'height');
+    }
+
     // Filter by scheme(s), if configured.
     if (isset($this->configuration['scheme'])) {
       $schemes = [];
@@ -72,6 +97,10 @@ class FileEntity extends FieldableEntity {
       'status' => $this->t('The published status of a file.'),
       'timestamp' => $this->t('The time that the file was added.'),
       'type' => $this->t('The type of this file.'),
+      'alt' => $this->t('Alt text of the file (if present)'),
+      'title' => $this->t('Title text of the file (if present)'),
+      'width' => $this->t('The width of the file (if present)'),
+      'height' => $this->t('The height of the file (if present)'),
     ];
   }
 

3c139898a7dd3b2670f52409e3b0bb8802b8d70b
Rich Lawson <rklawson@386645.no-reply.drupal.org>
2022-05-09 11:09:20 -0500

    NYSD9-245: phpstan fixes for SendGrid, per https://www.drupal.org/node/3159012.

--- web/modules/custom/nys_sendgrid/src/Plugin/Mail/Sendgrid.php
+++ web/modules/custom/nys_sendgrid/src/Plugin/Mail/Sendgrid.php
@@ -70,7 +70,7 @@ class Sendgrid implements MailInterface, ContainerFactoryPluginInterface {
   /**
    * A Sendgrid Mail object to represent the message being sent.
    *
-   * @var \Sendgrid\Mail\Mail
+   * @var \SendGrid\Mail\Mail
    */
   protected Mail $mailObj;
 
@@ -112,7 +112,7 @@ class Sendgrid implements MailInterface, ContainerFactoryPluginInterface {
   /**
    * Event Dispatcher service.
    *
-   * @var \Symfony\Component\EventDispatcher\EventDispatcherInterface
+   * @var \Symfony\Contracts\EventDispatcher\EventDispatcherInterface
    */
   protected EventDispatcherInterface $dispatcher;
 
@@ -132,7 +132,7 @@ class Sendgrid implements MailInterface, ContainerFactoryPluginInterface {
    *   A ModuleHandler service object.
    * @param \Drupal\Core\Config\ConfigFactory $config
    *   A ConfigFactory service object.
-   * @param \Symfony\Component\EventDispatcher\EventDispatcherInterface $dispatcher
+   * @param \Symfony\Contracts\EventDispatcher\EventDispatcherInterface $dispatcher
    *   An EventDispatcher service object.
    */
   public function __construct(\SendGrid $sendgrid, ModuleHandler $moduleHandler, ConfigFactory $config, EventDispatcherInterface $dispatcher) {

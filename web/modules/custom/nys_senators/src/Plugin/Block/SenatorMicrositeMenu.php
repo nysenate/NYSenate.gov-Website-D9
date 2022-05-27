c37f998f87b33d4c470d7870f3a2ab295cdeaac8
Wendy Stocker <wstocker@3287429.no-reply.drupal.org>
2022-05-27 15:11:13 -0500

    NYSD9-80: Implementation work in progress.

---
+++ web/modules/custom/nys_senators/src/Plugin/Block/SenatorMicrositeMenu.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drupal\nys_senators\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Block to generate the menu for all of the Senator Microsite Pages.
+ *
+ * @Block(
+ *   id = "nys_senators_microsite_menu",
+ *   admin_label = @Translation("Senator Microsite Menu"),
+ *   category = @Translation("NYS Senators")
+ * )
+ */
+class SenatorMicrositeMenu extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    /** @var Node $node */
+    $node = $this->getContextValue('node');
+    // Or we are on a District taxonomy term page.
+    if (!empty($node) && $node->getType() == 'microsite_page'
+     ) {
+      // Query to get all the "Microsite Pages" with the same Senator reference
+      // Get the Senator taxonomy reference from there populate links.
+      foreach ($nodes as $node) {
+        // Get the url alias for each and populate links for menu block
+        // $links[].
+      }
+      // Extract the Senator term from the node or taxonomy page to set the
+      // district populate the district link from the taxonomy term's url alias.
+      // Set up links so they can render the menu block template.
+    }
+  }
+
+}

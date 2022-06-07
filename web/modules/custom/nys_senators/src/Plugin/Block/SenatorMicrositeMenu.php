b546a64735125f84641dd2b140155ceee153d4d5
Wendy Stocker <wstocker@3287429.no-reply.drupal.org>
2022-06-07 11:21:36 -0500

    NYSD9-80: Adding custom block type 'Senator Microsite Menu' and dependencies.

--- web/modules/custom/nys_senators/src/Plugin/Block/SenatorMicrositeMenu.php
+++ web/modules/custom/nys_senators/src/Plugin/Block/SenatorMicrositeMenu.php
@@ -3,6 +3,9 @@
 namespace Drupal\nys_senators\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Session\AccountInterface;
 
 /**
  * Block to generate the menu for all of the Senator Microsite Pages.
@@ -22,6 +25,11 @@ class SenatorMicrositeMenu extends BlockBase {
     /** @var Node $node */
     $node = $this->getContextValue('node');
     if (!empty($node) && $node->getType() == 'microsite_page') {
+      $senator_terms = $node->get('field_senator_multiref')->getValue();
+      $nodes = \Drupal::entityTypeManager()->getStorage('node')->getQuery()
+        ->latestRevision()
+        ->condition('field_senator_multiref', $senator_terms)
+        ->execute();
       // Query to get all the "Microsite Pages" with the same Senator reference
       // Get the Senator taxonomy reference from there populate links.
       foreach ($nodes as $node) {
@@ -31,7 +39,33 @@ class SenatorMicrositeMenu extends BlockBase {
       // Extract the Senator term from the node or taxonomy page to set the
       // district populate the district link from the taxonomy term's url alias.
       // Set up links so they can render the menu block template.
+      return [
+        '#markup' => $this->t('Welcome page!'),
+      ];
     }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function blockAccess(AccountInterface $account) {
+    return AccessResult::allowedIfHasPermission($account, 'access content');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $config = $this->getConfiguration();
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $this->configuration['nys_senators_microsite_menu'] = $form_state->getValue('nys_senators_microsite_menu');
   }
 
 }

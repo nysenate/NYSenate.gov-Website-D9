a3467bc86cec4943cbcd7b046076f78e61435834
Wendy Stocker <wstocker@3287429.no-reply.drupal.org>
2022-06-08 09:20:48 -0500

    NYSD9-80: Adding custom block functionality.

--- web/modules/custom/nys_senators/src/Plugin/Block/SenatorMicrositeMenu.php
+++ web/modules/custom/nys_senators/src/Plugin/Block/SenatorMicrositeMenu.php
@@ -6,6 +6,12 @@ use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Session\AccountInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Cache\CacheBackendInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Routing\CurrentRouteMatch;
+use Drupal\node\Entity\Node;
 
 /**
  * Block to generate the menu for all of the Senator Microsite Pages.
@@ -13,34 +19,88 @@ use Drupal\Core\Session\AccountInterface;
  * @Block(
  *   id = "nys_senators_microsite_menu",
  *   admin_label = @Translation("Senator Microsite Menu"),
- *   category = @Translation("NYS Senators")
+ *   category = @Translation("NYS Senators"),
  * )
  */
-class SenatorMicrositeMenu extends BlockBase {
+class SenatorMicrositeMenu extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The CacheBackend Interface.
+   *
+   * @var \Drupal\Core\Cache\CacheBackendInterfacecache
+   */
+  protected $cache;
+
+  /**
+   * The Entity TypeManager Interfacee.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterfaceentityTypeManager
+   */
+  protected $entityTypeManager;
+
+
+  /**
+   * Current Route Match.
+   *
+   * @var Drupal|Core|Routing|CurrentRouteMatch
+   */
+  protected $routeMatch;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static($configuration, $plugin_id, $plugin_definition,
+      $container->get('cache.default'),
+      $container->get('entity_type.manager'),
+      $container->get('current_route_match'));
+  }
+
+  /**
+   * Constructor.
+   */
+  public function __construct(array $configuration,
+  $plugin_id,
+  $plugin_definition,
+  CacheBackendInterface $cache_backend,
+                              EntityTypeManagerInterface $entity_type_manager,
+  CurrentRouteMatch $route_match) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->cache = $cache_backend;
+    $this->entityTypeManager = $entity_type_manager;
+    $this->routeMatch = $route_match;
+  }
 
   /**
    * {@inheritdoc}
    */
   public function build() {
-    /** @var Node $node */
-    $node = $this->getContextValue('node');
+    /** @var \Drupal\node\Entity\Node $node */
+    $node = $this->routeMatch->getParameter('node');
     if (!empty($node) && $node->getType() == 'microsite_page') {
       $senator_terms = $node->get('field_senator_multiref')->getValue();
-      $nodes = \Drupal::entityTypeManager()->getStorage('node')->getQuery()
-        ->latestRevision()
-        ->condition('field_senator_multiref', $senator_terms)
-        ->execute();
-      // Query to get all the "Microsite Pages" with the same Senator reference
-      // Get the Senator taxonomy reference from there populate links.
-      foreach ($nodes as $node) {
-        // Get the url alias for each and populate links for menu block.
-        $links[] = $node->toUrl()->toString();
+      $tids = [];
+      foreach ($senator_terms as $tid) {
+        $tids[] = (int) $tid['target_id'];
       }
-      // Extract the Senator term from the node or taxonomy page to set the
-      // district populate the district link from the taxonomy term's url alias.
-      // Set up links so they can render the menu block template.
+      // Get all 'Microsite Pages' with the same senator reference.
+      $nids = $this->entityTypeManager->getStorage('node')->getQuery()
+        ->condition('field_senator_multiref', $tids)
+        ->execute();
+      $nodes = Node::loadMultiple($nids);
+      $menu_links = [];
+      foreach ($nodes as $node) {
+        $term = $node->get('field_microsite_page_type')->entity;
+        $menu_title = $term->getName();
+        $menu_weight = $term->get('field_microsite_menu_weight')->value;
+        // Get the url alias for each 'Microsite Page' and populate
+        // links for menu block.
+        $menu_links[$menu_weight]['menu_url'] = $node->toUrl()->toString();
+        $menu_links[$menu_weight]['menu_title'] = $menu_title;
+      }
       return [
-        '#markup' => $this->t('Welcome page!'),
+        '#theme' => 'senator_microsite_menu_block',
+        '#menu_links' => $menu_links,
       ];
     }
   }

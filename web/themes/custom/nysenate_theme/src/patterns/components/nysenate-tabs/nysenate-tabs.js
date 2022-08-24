a5017068a616211f3b648a10623427805ede91cc
Patrick Ace Buen <patrick.ace.buen@codeandtheory.com>
2022-08-24 22:06:29 +0800

    NYSD9-220: WIP -- Integrated the latest updates component

--- web/themes/custom/nysenate_theme/src/patterns/components/nysenate-tabs/nysenate-tabs.js
+++ web/themes/custom/nysenate_theme/src/patterns/components/nysenate-tabs/nysenate-tabs.js
@@ -3,7 +3,7 @@
   Drupal.behaviors.nysenateTabs = {
     attach: function () {
       const tabContainer = $('.l-tab-bar');
-      const tabLink =  $('.c-tab .c-tab-link');
+      const tabLink = $('.c-tab .c-tab-link');
 
       tabContainer.each(function () {
         const tabArrowDown = $(this).find('.c-tab--arrow');
@@ -14,28 +14,37 @@
         }
 
         tabInput.on('click', function () {
+          const tabInputContainer = tabInput.parent();
+          const tabContent = tabInputContainer
+            .parent()
+            .parent()
+            .find('.tabs-content');
+
           tabInput.removeAttr('checked');
-          tabInput.parent().removeClass('active');
+          tabInputContainer.removeClass('active');
 
           $(this).attr('checked', 'checked');
           $(this).parent().addClass('active');
+
+          tabContent.find('.active').removeClass('active');
+          tabContent.find($(this).val()).addClass('active');
         });
       });
 
       tabLink.on('click', this.toggleTabDropdown);
     },
-    toggleTabDropdown: function(e) {
+    toggleTabDropdown: function (e) {
       e.preventDefault();
 
       var tab = $(this).parent('.c-tab');
       var tabBar = $(this).parents('.l-tab-bar');
 
-      if(tab.hasClass('active') && !tabBar.hasClass('open')) {
+      if (tab.hasClass('active') && !tabBar.hasClass('open')) {
         tabBar.addClass('open');
       }
       else {
         tabBar.removeClass('open');
       }
-    },
+    }
   };
 })(document, Drupal, jQuery);

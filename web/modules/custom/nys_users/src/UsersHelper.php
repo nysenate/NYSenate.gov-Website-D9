b2feb3c5bf1050e3922335788164938370fb9dd5
Patrick Ace Buen <patrick.ace.buen@codeandtheory.com>
2022-09-05 16:36:32 +0800

    NYSD9-370: Port the required method for the contact your senator component

--- web/modules/custom/nys_users/src/UsersHelper.php
+++ web/modules/custom/nys_users/src/UsersHelper.php
@@ -50,7 +50,8 @@ class UsersHelper {
    * @return bool
    *   whether given user is an out of state user.
    */
-  public function isOutOfState($user = '') {
+  public function isOutOfState($user) {
+    $uid = '';
     if (is_object($user)) {
       $uid = $user->id();
     }
@@ -61,18 +62,20 @@ class UsersHelper {
       $uid = $this->currentUser->id();
     }
 
-    $district_tid = user_get_district_tid($uid);
+    $district_tid = $this->getDistrictTid($uid);
 
     if (empty($district_tid)) {
       return TRUE;
     }
     else {
       // Check if user's address is out of state.
-      $query = "SELECT 1 FROM field_data_field_address fa
-        JOIN location l on l.lid = fa.field_address_lid
-        WHERE fa.entity_id = :uid AND fa.bundle = 'user' AND l.province != 'NY' LIMIT 1";
-      $oos = (int) db_query($query, array(':uid' => $uid))->FetchField();
-      return $oos;
+      $user = $this->entityTypeManagerInterface->getStorage('user')
+        ->load($uid);
+
+      if ($user->hasField('field_address') && !$user->get('field_address')->isEmpty()) {
+        $address = $user->get('field_address')->getValue();
+        return $address[0]['administrative_area'] != 'NY' ? TRUE : FALSE;
+      }
     }
 
     return FALSE;
@@ -87,7 +90,8 @@ class UsersHelper {
    * @return bool
    *   Whether given user refers to a user who is a Senator.
    */
-  public function isSenator() {
+  public function isSenator($user) {
+    $uid = '';
     if (is_object($user)) {
       $uid = $user->uid;
     }
@@ -95,9 +99,9 @@ class UsersHelper {
       $uid = $user;
     }
     elseif (empty($user->uid)) {
-      $uid = $this->currentUser()->id();
+      $uid = $this->currentUser->id();
     }
-  
+
     if (!is_object($user)) {
       $user = $this->entityTypeManagerInterface->getStorage('user')
         ->load($uid);
@@ -105,16 +109,18 @@ class UsersHelper {
     if ($user->hasRole('senator')) {
       return TRUE;
     }
-  
+
     return FALSE;
   }
 
   /**
    * Given a $user object or $uid, return that user's district's tid.
    *
-   * @param $user user object or user id
+   * @param int|object $user
+   *   User object or user id.
    *
-   * @return int district taxonomy term id attached to the user object
+   * @return int
+   *   District taxonomy term id attached to the user object
    */
   public function getDistrictTid($user) {
 
@@ -129,14 +135,16 @@ class UsersHelper {
   /**
    * Get user's district and senator identifiers.
    *
-   * Given a $user object or $uid, return that user's district and that district's
-   * senator's identifiers, used by other utility functions in this module,
+   * Given a $user object or $uid, return that user's district
+   * and that district's senator's identifiers, used
+   * by other utility functions in this module,
    * as not to repeat code.
    *
-   * TODO: This function is inappropriately named.
-   * It does not actually load the district info from the user, but rather loads
-   * the user's senator, then loads the district from the senator object.  This
-   * causes a failure if the district does not have an assigned senator (e.g., an
+   * @todo This function is inappropriately named.
+   * It does not actually load the district info from the user,
+   * but rather loads the user's senator, then loads
+   * the district from the senator object. This causes a
+   * failure if the district does not have an assigned senator (e.g., an
    * empty seat).  We are leaving this in place for now because it is already
    * in wide usage.  See user_get_district_data() to load the district info
    * properly from the user information.
@@ -149,6 +157,7 @@ class UsersHelper {
    */
   public function getDistrictSenatorDataArray($user) {
     $user_district_senator_info = &drupal_static(__FUNCTION__);
+    $uid = '';
     if (is_object($user)) {
       $uid = $user->uid;
     }
@@ -156,31 +165,40 @@ class UsersHelper {
       $uid = $user;
     }
     elseif (empty($user->uid)) {
-      $uid = $this->currentUser()->id();
+      $uid = $this->currentUser->id();
     }
+
+    $senator = NULL;
+    $user = $this->entityTypeManagerInterface->getStorage('user')
+      ->load($uid);
 
     if (!isset($user_district_senator_info[$uid])) {
       if ($this->isSenator($uid)) {
-        $query = "SELECT ua.field_user_account_target_id AS senator_uid,
-          ua.entity_id AS senator_nid, s.entity_id AS district_tid
-          FROM field_data_field_user_account ua
-          JOIN field_data_field_senator s ON s.field_senator_target_id = ua.entity_id AND s.bundle = 'districts'
-          WHERE ua.field_user_account_target_id = :uid;";
-
-        $user_district_senator_info[$uid] = $this->connection->query($query, [':uid' => $uid])->fetchAssoc();
+        // Assign the current user as the senator.
+        $senator = $user;
       }
       else {
-        $query = "SELECT ua.field_user_account_target_id AS senator_uid,
-          s.field_senator_target_id AS senator_nid,
-          fd.field_district_target_id AS district_tid
-          FROM field_data_field_district fd
-          JOIN field_data_field_senator s ON s.entity_id = fd.field_district_target_id
-          JOIN field_data_field_user_account ua ON ua.entity_id = s.field_senator_target_id
-          WHERE fd.entity_id = :uid and fd.bundle = 'user' and s.bundle = 'districts';";
-
-        $user_district_senator_info[$uid] = $this->connection->query($query, [':uid' => $uid])->fetchAssoc();
+        if ($user->hasField('field_senator_multiref') && !$user->get('field_senator_multiref')->isEmpty()) {
+          $tid = $user->get('field_senator_multiref')->first()->getString() ?? '';
+          if (!empty($tid)) {
+            $senator = $this->entityTypeManagerInterface->getStorage('user')
+              ->load($tid);
+          }
+        }
       }
     }
+
+    if (!empty($senator)) {
+      $query = "SELECT fs.entity_id as district_tid FROM taxonomy_term__field_senator fs WHERE fs.field_senator_target_id = :sid AND fs.bundle = :bundle";
+      $result = $this->connection->query($query, [
+        ':sid' => $senator->id(),
+        ':bundle' => 'districts',
+      ])->fetchAssoc();
+
+      $user_district_senator_info[$uid] = $result;
+    }
+
     return $user_district_senator_info[$uid];
   }
+
 }

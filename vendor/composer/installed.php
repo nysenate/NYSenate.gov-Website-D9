33b545a2f735f921f0a2ce87ebd460f5568674ba
DDEV-Local User <nobody@example.com>
2022-09-13 18:35:16 +0000

    master build at 2022-09-13T18:35:16+00:00 Merged in release/1.0.7 (pull request #437)
    Release/1.0.7

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -5,7 +5,7 @@
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '846a529ba0fbd5c4c222a61d0396c0d1246ec3d6',
+        'reference' => 'f7cb4853f6ee20f64b9da1c5afe98c38390a268c',
         'name' => 'mediacurrent/drupal-project',
         'dev' => false,
     ),
@@ -1873,7 +1873,7 @@
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => '846a529ba0fbd5c4c222a61d0396c0d1246ec3d6',
+            'reference' => 'f7cb4853f6ee20f64b9da1c5afe98c38390a268c',
             'dev_requirement' => false,
         ),
         'mediacurrent/mis_profile' => array(

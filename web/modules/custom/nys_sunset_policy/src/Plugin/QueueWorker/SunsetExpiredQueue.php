98dacc356ccbe485e86125b479a0e5b1af6ff251
Wendy Stocker <wstocker@3287429.no-reply.drupal.org>
2022-11-17 14:54:38 -0600

    NYSD9-185: Work in progress

--- web/modules/custom/nys_sunset_policy/src/Plugin/QueueWorker/SunsetExpiredQueue.php
+++ web/modules/custom/nys_sunset_policy/src/Plugin/QueueWorker/SunsetExpiredQueue.php
@@ -18,97 +18,93 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 * )
 */
 final class SunsetExpiredQueue extends QueueWorkerBase implements ContainerFactoryPluginInterface {
-  /**
-   * The entity type manager.
-   *
-   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
-   */
-  protected $entityTypeManager;
+/**
+ * The entity type manager.
+ *
+ * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+ */
+protected $entityTypeManager;
 
-  /**
-   * The database connection.
-   *
-   * @var \Drupal\Core\Database\Connection
-   */
-  protected $database;
+/**
+ * The database connection.
+ *
+ * @var \Drupal\Core\Database\Connection
+ */
+protected $database;
 
-  /**
-   * Main constructor.
-   *
-   * @param array $configuration
-   *   Configuration array.
-   * @param mixed $plugin_id
-   *   The plugin id.
-   * @param mixed $plugin_definition
-   *   The plugin definition.
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
-   *   The entity type manager.
-   * @param \Drupal\Core\Database\Connection $database
-   *   The connection to the database.
-   */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, Connection $database) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition);
-    $this->entityTypeManager = $entity_type_manager;
-    $this->database = $database;
-  }
+/**
+ * Main constructor.
+ *
+ * @param array $configuration
+ *   Configuration array.
+ * @param mixed $plugin_id
+ *   The plugin id.
+ * @param mixed $plugin_definition
+ *   The plugin definition.
+ * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+ *   The entity type manager.
+ * @param \Drupal\Core\Database\Connection $database
+ *   The connection to the database.
+ */
+public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, Connection $database) {
+  parent::__construct($configuration, $plugin_id, $plugin_definition);
+  $this->entityTypeManager = $entity_type_manager;
+  $this->database = $database;
+}
 
-  /**
-   * Used to grab functionality from the container.
-   *
-   * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
-   *   The container.
-   * @param array $configuration
-   *   Configuration array.
-   * @param mixed $plugin_id
-   *   The plugin id.
-   * @param mixed $plugin_definition
-   *   The plugin definition.
-   *
-   * @return static
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static(
-     $configuration,
-     $plugin_id,
-     $plugin_definition,
-     $container->get('entity_type.manager'),
-     $container->get('database'),
-    );
-  }
+/**
+ * Used to grab functionality from the container.
+ *
+ * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+ *   The container.
+ * @param array $configuration
+ *   Configuration array.
+ * @param mixed $plugin_id
+ *   The plugin id.
+ * @param mixed $plugin_definition
+ *   The plugin definition.
+ *
+ * @return static
+ */
+public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+  return new static(
+   $configuration,
+   $plugin_id,
+   $plugin_definition,
+   $container->get('entity_type.manager'),
+   $container->get('database'),
+  );
+}
 
-  /**
-   * Processes an item in the queue.
-   *
-   * @param mixed $data
-   *   The queue item data.
-   *
-   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
-   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
-   * @throws \Drupal\Core\Entity\EntityStorageException
-   * @throws \Exception
-   */
-  public function processItem($data) {
-    $nid = $data->nid;
-    $update = $data->update;
+/**
+ * Processes an item in the queue.
+ *
+ * @param mixed $data
+ *   The queue item data.
+ *
+ * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+ * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+ * @throws \Drupal\Core\Entity\EntityStorageException
+ * @throws \Exception
+ */
+public function processItem($data) {
+  $nid = $data->nid;
 
-    // Processing of queue items logic goes here.
-    $mailManager = \Drupal::service('plugin.manager.mail');
-    $params = $data;
-    $mailManager->mail('learning', 'email_queue', $data['email'], 'en', $params , $send = TRUE);
-  }
+  // Processing of queue items logic goes here.
+  $mailManager = \Drupal::service('plugin.manager.mail');
+  $params = $data;
+  $subject = "Content will expire soon - " . $node_wrapper->title->value();
+  $expire_date = date('l M jS Y', $node_wrapper->field_expiration_date->value());
+  $notice = "This item is set to be unpublished from the New York Senate web site on " . $expire_date;
+  $mailManager = \Drupal::service('plugin.manager.mail');
 
- /**
-   * Add expired item in the queue.
-   *
-   * @param mixed $data
-   *   The queue item data.
-   *
-   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
-   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
-   * @throws \Drupal\Core\Entity\EntityStorageException
-   * @throws \Exception
-   */
-  public function createExpiredItem() {
-
+  $to = $data['to'];
+  $params['message'] = $orderData;
+  $params['title'] = $subject;
+  // The email field can contain multiple values.
+  foreach ($senator_emails as $senator_email) {
+    $params = ['subject' => $subject, 'body' => $body];
+    $result = $mailManager->mail('nys_sunset_policy', 'expired_mail', $to, NULL, $params, NULL, TRUE);
+    ;
   }
 }

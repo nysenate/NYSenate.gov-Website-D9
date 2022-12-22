5310a93d3c3e0ba8ec7334c451b8c5de1f1d4391
Angelo Ian Reyes <angelo.ian.reyes@codeandtheory.com>
2022-12-22 20:11:04 +0800

    NYSD9-405: code clean up

--- web/themes/custom/nysenate_theme/src/patterns/components/dashboard-inbox/dashboard-inbox.es6.js
+++ web/themes/custom/nysenate_theme/src/patterns/components/dashboard-inbox/dashboard-inbox.es6.js
@@ -13,84 +13,21 @@
    */
   Drupal.behaviors.dashboardInbox = {
     attach: function (context) {
-      // eslint-disable-next-line no-console
-      console.log('hello');
-
-      const toggleBtn = $('.message-body-toggle', context);
+      const toggleBtn = $('.message-list__td--issue__toggle', context).once('messageList');
       toggleBtn.each(function () {
-        const actionBtns = $(this).parent().find('.message-action-buttons');
+        const actionBtns = $(this).parent().find('.message-list__td--issue__actions');
         $(this).click(function () {
 
           if (actionBtns.css('display') === 'flex') {
             actionBtns.css('display', 'none');
-            $(this).removeClass('message-body-toggle--expanded');
+            $(this).removeClass('message-list__td--issue__toggle--expanded');
           }
           else {
             actionBtns.css('display', 'flex');
-            $(this).addClass('message-body-toggle--expanded');
+            $(this).addClass('message-list__td--issue__toggle--expanded');
           }
         });
       });
-
-      // listIssue.each(function () {
-      //   console.log('hello');
-      // });
-
-      // $(context).once('.privatemsg-list-issue').on('click');
-
-      // once(
-      //   'initPrivateMsgListIssue',
-      //   '.privatemsg-list-issue',
-      //   context
-      // ).forEach(function (element) {
-      //   const toggleBtn = $(element).find('.message-body-toggle');
-
-      //   toggleBtn.click(function (el) {
-      //     $(el).css('background', 'red');
-      //     // alert('clicked!');
-      //     // console.log('clicked');
-      //   });
-      //   // $(element).
-      // });
-
-      // once(
-      //   'initPrivateMsgListIssue',
-      //   '.privatemsg-list-issue',
-      //   context
-      // ).forEach(function (element) {
-      //   const toggleBtn = $(element).find('.message-body-toggle');
-
-      //   toggleBtn.click(function (el) {
-      //     $(el).css('background', 'red');
-      //     // alert('clicked!');
-      //     // console.log('clicked');
-      //   });
-      //   // $(element).
-      // });
-
-      //   const self = this;
-      //   const heroContainer = $('.hero--homepage');
-      //   let pageMargin = ($('main').outerWidth(true) - $('main').outerWidth()) / 2;
-      //   let pagePadding = ($('main').innerWidth() - $('main').width()) / 2;
-
-      //   self.heroMargin(heroContainer, pageMargin, pagePadding);
-
-      //   $(window).on('resize', function () {
-      //     pageMargin = ($('main').outerWidth(true) - $('main').outerWidth()) / 2;
-      //     pagePadding = ($('main').innerWidth() - $('main').width()) / 2;
-
-      //     self.heroMargin(heroContainer, pageMargin, pagePadding);
-      //   });
-      // },
-      // heroMargin: function (heroContainer, pageMargin, pagePadding) {
-      //   if ($(window).width() >= 1500) {
-      //     heroContainer.css('margin-left', `-${1500 / 4 }px`);
-      //   }
-      //   else {
-      //     heroContainer.css('margin-left', `-${pageMargin + pagePadding}px`);
-      //   }
     }
   };
-
-  // eslint-disable-next-line no-undef
 })(document, Drupal, jQuery);

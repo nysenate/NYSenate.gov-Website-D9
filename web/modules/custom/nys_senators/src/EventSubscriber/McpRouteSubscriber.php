713d1e3f25078d7b0032e0c67f493f7539ca6e7f
Mike Wilbur <michael.wilbur@mediacurrent.com>
2023-02-21 14:05:21 -0500

    NYSD9-527 MCP Access
    WIP Branch. Updated Grants access, and entity access for taxonomy terms
    and nodes across the board for MCPs. Added some route controlling and
    view all revisions access for MCPs. Started some views query tagging to
    help with content visibility for MCPs.

---
+++ web/modules/custom/nys_senators/src/EventSubscriber/McpRouteSubscriber.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\nys_senators\EventSubscriber;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Class McpRouteSubscriber for adding custom access control to routes.
+ *
+ * @package Drupal\nys_senators\EventSubscriber
+ *
+ * Removes MCP access to admin block pages to mitigate administer blocks perm.
+ */
+class McpRouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function alterRoutes(RouteCollection $collection): void {
+
+    // Custom access to block admin routes.
+    if ($route = $collection->get('block.admin_display')) {
+      $route->setRequirement('_custom_access', 'Drupal\nys_senators\Access\McpAccessCheck::access');
+    }
+    if ($route = $collection->get('block_content.add_page')) {
+      $route->setRequirement('_custom_access', 'Drupal\nys_senators\Access\McpAccessCheck::access');
+    }
+    if ($route = $collection->get('entity.block_content_type.collection')) {
+      $route->setRequirement('_custom_access', 'Drupal\nys_senators\Access\McpAccessCheck::access');
+    }
+    if ($route = $collection->get('entity.block_content.collection')) {
+      $route->setRequirement('_custom_access', 'Drupal\nys_senators\Access\McpAccessCheck::access');
+    }
+  }
+
+}

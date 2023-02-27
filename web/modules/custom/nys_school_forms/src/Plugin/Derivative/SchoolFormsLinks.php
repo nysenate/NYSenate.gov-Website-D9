a63904a493383d0c5f9e712f4becf8cd3efc5de4
Brian Flores <brian.flores@codeandtheory.com>
2023-02-27 23:07:31 +0800

    School forms BE

---
+++ web/modules/custom/nys_school_forms/src/Plugin/Derivative/SchoolFormsLinks.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Drupal\nys_school_forms\Plugin\Derivative;
+
+use Drupal\Component\Plugin\Derivative\DeriverBase;
+use Drupal\Core\Plugin\Discovery\ContainerDeriverInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Derivative class that provides the menu links for the Products.
+ */
+class SchoolFormsLinks extends DeriverBase implements ContainerDeriverInterface {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct($base_plugin_id, EntityTypeManagerInterface $entity_type_manager) {
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, $base_plugin_id) {
+    return new static(
+      $base_plugin_id,
+      $container->get('entity_type.manager')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDerivativeDefinitions($base_plugin_definition) {
+    $links = [];
+
+    $school_form_types = $this->entityTypeManager->getStorage('taxonomy_term')->loadByProperties(['vid' => 'school_form_type']);
+    foreach ($school_form_types as $form_type) {
+      $term_name = $form_type->getName();
+      $alias = str_replace([' ', '-', '\''], '_', strtolower($term_name));
+
+      $links[$form_type->id()] = [
+        'title' => 'School Form Submission - ' . $term_name,
+        'description' => 'List School Form Submissions',
+        'route_name' => 'nys_school_forms.school_forms.' . $alias,
+        'parent' => 'entity.webform.collection',
+        'route_parameters' => ['form_type' => $term_name],
+      ] + $base_plugin_definition;
+    }
+
+    return $links;
+  }
+
+}

f4e389eb4f8ec392f7d1279058a735a8781b3dfb
Mike Wilbur <michael.wilbur@mediacurrent.com>
2023-02-27 10:53:29 -0500

    NYSD9-527 MCP Access
    Updated Entity Access logic. Moved admin checker into SenatorsHelper
    service. Added event subscriber to redirect users away from Senator
    terms if they don't have access. Removed bad logic from access
    permissions module. Seperated views query alter in execution include.
    Finalized permissions to MCPs. Added term view for further alteration to
    remove taxonomy overview permissions to MCPs.

---
+++ web/modules/custom/nys_senators/src/EventSubscriber/McpEventSubscriber.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace Drupal\nys_senators\EventSubscriber;
+
+use Drupal\Core\Http\RequestStack;
+use Drupal\Core\Routing\CurrentRouteMatch;
+use Drupal\Core\Routing\TrustedRedirectResponse;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\user\Entity\User;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Class McpEventSubscriber for adding custom redirects control to terms.
+ *
+ * @package Drupal\nys_senators\EventSubscriber
+ *
+ * Removes non-admin/mcp access to Senator Terms.
+ */
+class McpEventSubscriber implements EventSubscriberInterface {
+
+  /**
+   * The request stack.
+   *
+   * @var \Symfony\Component\HttpFoundation\RequestStack
+   */
+  protected $requestStack;
+
+  /**
+   * The current route match.
+   *
+   * @var \Drupal\Core\Routing\CurrentRouteMatch
+   */
+  protected $currentRouteMatch;
+
+  /**
+   * The current user.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  protected AccountInterface $currentUser;
+
+  /**
+   * A Senator Helper Service.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  protected $senatorHelper;
+
+  /**
+   * McpEventSubscriber constructor.
+   *
+   * @param \Symfony\Component\HttpFoundation\RequestStack $request_stack
+   *   The request stack.
+   * @param \Drupal\Core\Routing\CurrentRouteMatch $current_route_match
+   *   The current route match.
+   * @param \Drupal\Core\Session\AccountInterface $current_user
+   *   The current user.
+   */
+  public function __construct(RequestStack $request_stack, CurrentRouteMatch $current_route_match, AccountInterface $current_user) {
+    $this->requestStack = $request_stack;
+    $this->currentRouteMatch = $current_route_match;
+    $this->currentUser = $current_user;
+    $this->senatorHelper = \Drupal::service('nys_senators.senators_helper');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function redirectFromSenators(RequestEvent $event) {
+
+    // Grab route name for checking.
+    $routeName = $this->currentRouteMatch->getRouteName();
+
+    // Check if a non-admin is looking at a Senator term.
+    if (!$this->senatorHelper->senatorUserIsAdmin($this->currentUser) &&
+      $routeName === 'entity.taxonomy_term.canonical' &&
+      ($term = $this->currentRouteMatch->getParameter('taxonomy_term'))->bundle() === 'senator') {
+
+      // Load current user in full.
+      $user = User::load($this->currentUser->id());
+      $has_mcp_access = FALSE;
+
+      // Check if user has access to term.
+      if ($user->hasField('field_senator_multiref') &&
+        !empty($senators = array_column($user->field_senator_multiref->getValue(), 'target_id')) &&
+        in_array($term->id(), $senators, FALSE)) {
+        // User is MCP with access to term.
+        $has_mcp_access = TRUE;
+      }
+
+      // If no MCP access then redirect.
+      if (!$has_mcp_access) {
+        // Redirecting Home.
+        $response = new TrustedRedirectResponse('/', 302);
+        $event->setResponse($response);
+      }
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events = [
+      KernelEvents::REQUEST => 'redirectFromSenators',
+    ];
+    return $events;
+  }
+
+}

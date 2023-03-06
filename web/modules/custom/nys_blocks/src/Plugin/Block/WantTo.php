65140ea795242b1ad993bb976038e00cd746457c
aheaphy <aheaphy@outlook.com>
2023-03-06 11:18:52 -0500

    Custom block for home page I want to block, plus styles

---
+++ web/modules/custom/nys_blocks/src/Plugin/Block/WantTo.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Drupal\nys_blocks\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\user\Entity\User;
+use Drupal\Core\Url;
+
+/**
+ * Block for How a Bill Becomes a Law.
+ *
+ * @Block(
+ *   id = "nys_blocks_want_to",
+ *   admin_label = @Translation("I want to"),
+ * )
+ */
+class WantTo extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheMaxAge(): int {
+    return 1800;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $logged_in = \Drupal::currentUser()->isAuthenticated();
+    if ($logged_in) {
+      $current_user = \Drupal::currentUser();
+      $user = User::load($current_user->id());
+      $senator = $user->field_district->entity->field_senator;
+      $senator_link = \Drupal::service('nys_senators.microsites')->getMicrosites($senator);
+      $headshot_id = $user->field_district->entity->field_senator
+        ->entity->field_member_headshot->target_id;
+      $headshot = \Drupal::entityTypeManager()->getStorage('media')
+        ->load($headshot_id);
+      $headshot = \Drupal::entityTypeManager()
+        ->getViewBuilder('media')
+        ->view($headshot, 'thumbnail');
+    }
+    $register = Url::fromRoute('user.register')->toString();
+
+    return [
+      '#theme' => 'nys_blocks_want_to',
+      '#headshot' => $headshot ?? NULL,
+      '#senator_link' => $senator_link ?? NULL,
+      '#register' => $register,
+      '#cache' => [
+        'contexts' => ['user'],
+      ],
+    ];
+  }
+
+}

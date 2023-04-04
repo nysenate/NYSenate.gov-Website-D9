9259c7fd1db8c18619a1081ccb6e26b6cb6cd3d5
Rich Lawson <richlawson@users.noreply.github.com>
2023-04-04 16:25:50 -0500

    NYSD9-648: Minor fix for senator multiref field and added plugin for mapping the D7 field_senator_inbox_access field data into D9 via field_user_account field on the relevant Senator taxonomy term.

---
+++ web/modules/custom/nys_migrate/src/Plugin/migrate/process/SenatorUserMigrateMapLookup.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drupal\nys_migrate\Plugin\migrate\process;
+
+use Drupal\migrate\Row;
+use Drupal\Core\Database\Database;
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\MigrateExecutableInterface;
+
+/**
+ * The process plugin for getting the senator term id.
+ *
+ * @MigrateProcessPlugin(
+ *   id = "senator_user_migrate_map_lookup",
+ *   source_module = "nys_migrate"
+ * )
+ */
+class SenatorUserMigrateMapLookup extends ProcessPluginBase {
+
+  /**
+   * {@inheritdoc}
+   *
+   * Retrieve the senator taxonomy term ID.
+   */
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+
+    // Initialize connection.
+    $db = Database::getConnection();
+
+    // Query for the Senator taxonomy term referencing the same user account.
+    $query = $db->select('taxonomy_term__field_user_account', 'e')
+      ->fields('e', ['entity_id']);
+    $query->condition('field_user_account_target_id', $value['target_id']);
+    $result = $query->execute()->fetchAssoc();
+
+    return $result['entity_id'];
+  }
+
+}

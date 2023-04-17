8f33efb1be5d93b277eb2e68b8f59c0096b295d7
Ben Ray-An Donor <ben.ray.an.donor@codeandtheory.com>
2023-04-17 16:53:00 +0800

    NYSD9-401: Created Senator Profile display view for Senator Taxonomy Terms. Created a template for the Senator Profile view mode. Rendered the Message Senator Form. This still needs FE styling and modal functionality.

--- web/modules/custom/nys_dashboard/src/Controller/DashboardController.php
+++ web/modules/custom/nys_dashboard/src/Controller/DashboardController.php
@@ -14,9 +14,21 @@ class DashboardController extends ControllerBase {
    * Controller method for the entrance page.
    */
   public function overview() {
+    $build = [];
     $user = UsersHelper::resolveUser($this->currentUser());
-    $build['content'] = \Drupal::service('entity_type.manager')
-      ->getViewBuilder('user')->view($user, 'dashboard_profile');
+    if ($user) {
+      $build['content']['user'] = \Drupal::service('entity_type.manager')
+        ->getViewBuilder('user')->view($user, 'dashboard_profile');
+
+      // Get User's District Senator.
+      $senator = UsersHelper::getSenator($user);
+      if ($senator) {
+        $build['content']['my_senator'] = \Drupal::service('entity_type.manager')
+          ->getViewBuilder('taxonomy_term')->view($senator, 'senator_profile');
+        $build['content']['message_form'] = \Drupal::service('form_builder')
+          ->getForm('Drupal\nys_messaging\Form\SenatorMessageForm', $user->id());
+      }
+    }
 
     return $build;
   }

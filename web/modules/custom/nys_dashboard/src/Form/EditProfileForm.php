638b7235fd388d17f3d28fa0ebbaf76e814a8580
Ben Ray-An Donor <ben.ray.an.donor@codeandtheory.com>
2023-04-18 19:03:01 +0800

    NYSD9-401: WIP - Creating a Custom Edit Profile Form.

--- web/modules/custom/nys_dashboard/src/Form/EditProfileForm.php
+++ web/modules/custom/nys_dashboard/src/Form/EditProfileForm.php
@@ -2,13 +2,51 @@
 
 namespace Drupal\nys_dashboard\Form;
 
-use Drupal\Core\Form\FormBase;
+use Drupal\Component\Datetime\TimeInterface;
+use Drupal\Core\Entity\EntityRepositoryInterface;
+use Drupal\Core\Entity\EntityTypeBundleInfoInterface;
+use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Language\LanguageManagerInterface;
+use Drupal\user\ProfileForm;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Form to edit a user's profile.
  */
-class EditProfileForm extends FormBase {
+class EditProfileForm extends ProfileForm {
+
+  /**
+   * The module handler service.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
+  protected $moduleHandler;
+
+  /**
+   * {@inheritDoc}
+   */
+  public function __construct(EntityRepositoryInterface $entity_repository, LanguageManagerInterface $language_manager, ModuleHandlerInterface $moduleHandler, EntityTypeBundleInfoInterface $entity_type_bundle_info = NULL, TimeInterface $time = NULL) {
+    $user = \Drupal::entityTypeManager()->getStorage('user')
+      ->load(\Drupal::currentUser()->id());
+    $this->setEntity($user);
+
+    parent::__construct($entity_repository, $language_manager, $entity_type_bundle_info, $time);
+    $this->moduleHandler = $moduleHandler;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container): self {
+    return new static(
+      $container->get('entity.repository'),
+      $container->get('language_manager'),
+      $container->get('module_handler'),
+      $container->get('entity_type.bundle.info'),
+      $container->get('datetime.time')
+    );
+  }
 
   /**
    * {@inheritDoc}
@@ -21,9 +59,8 @@ class EditProfileForm extends FormBase {
    * {@inheritDoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state): array {
-    $form['stuff'] = [
-      '#markup' => '<h1>Edit Profile Form</h1>',
-    ];
+    $form = parent::buildForm($form, $form_state);
+
     return $form;
   }
 

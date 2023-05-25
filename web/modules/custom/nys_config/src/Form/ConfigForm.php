53d6d3110dc2673dfc3254cfef74fcad009f3199
Ben Ray-An Donor <ben.ray.an.donor@codeandtheory.com>
2023-05-25 23:48:48 +0800

    NYSD9-751: WIP - Config Form updates.

--- web/modules/custom/nys_config/src/Form/ConfigForm.php
+++ web/modules/custom/nys_config/src/Form/ConfigForm.php
@@ -5,6 +5,7 @@ namespace Drupal\nys_config\Form;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\file\Entity\File;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -40,8 +41,10 @@ class ConfigForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-
     $config = $this->config('nys_config.settings');
+    $validators = [
+      'file_validate_extensions' => ['pdf'],
+    ];
 
     /*
      * GOVERNOR.
@@ -116,13 +119,74 @@ class ConfigForm extends ConfigFormBase {
       '#size' => 4,
       '#default_value' => $config->get('nys_session_year'),
     ];
+    $form['session_state']['nys_senate_status'] = [
+      '#title' => $this->t('Custom NY State Senate Status Text'),
+      '#type'  => 'textfield',
+      '#size' => 35,
+      '#default_value' => $config->get('nys_senate_status'),
+    ];
+    $form['session_state']['nys_vid_redir_ip'] = [
+      '#title' => $this->t('Video Redirect IP'),
+      '#description' => $this->t('Enter an IP (or lower IP value if a range of addresses) in the format 123.456.789.012 that should be redirected to internal feeds when streaming video. Supports wildcards (123.456.*.*).'),
+      '#type'  => 'textfield',
+      '#size'  => 15,
+      '#default_value' => $config->get('nys_vid_redir_ip'),
+    ];
+    $form['session_state']['nys_vid_redir_ip_2'] = [
+      '#title' => $this->t('Video Redirect IP Range (Optional)'),
+      '#description' => $this->t('If a range of addresses is being used, enter the top value of IP addresses. Supports wildcards (123.456.789.*).'),
+      '#type'  => 'textfield',
+      '#size'  => 15,
+      '#default_value' => $config->get('nys_vid_redir_ip_2'),
+    ];
+    $form['session_state']['nys_vid_redir_url'] = [
+      '#title' => $this->t('Video Redirect URL'),
+      '#description' => $this->t('Enter the URL that IPs streaming internal video should be directed to.'),
+      '#type'  => 'textfield',
+      '#default_value' => $config->get('nys_vid_redir_url'),
+    ];
+
+    /*
+     * PDF contents.
+     */
+    $form['pdfs'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('PDFs'),
+    ];
+    $form['pdfs']['member_listing_pdf'] = [
+      '#type' => 'managed_file',
+      '#multiple' => FALSE,
+      '#name' => 'member_listing_pdf',
+      '#title' => $this->t('Member Listing PDF'),
+      '#default_value' => $config->get('member_listing_pdf') ?? NULL,
+      '#upload_location' => 'public://',
+      '#upload_validators' => $validators,
+    ];
+    $form['pdfs']['session_calendar_pdf'] = [
+      '#type' => 'managed_file',
+      '#multiple' => FALSE,
+      '#name' => 'session_calendar_pdf',
+      '#title' => $this->t('Session Calendar PDF'),
+      '#default_value' => $config->get('session_calendar_pdf') ?? NULL,
+      '#upload_location' => 'public://',
+      '#upload_validators' => $validators,
+    ];
+    $form['pdfs']['public_hearing_schedule'] = [
+      '#type' => 'managed_file',
+      '#multiple' => FALSE,
+      '#name' => 'public_hearing_schedule',
+      '#title' => $this->t('Public Hearing Schedule PDF'),
+      '#default_value' => $config->get('public_hearing_schedule') ?? NULL,
+      '#upload_location' => 'public://',
+      '#upload_validators' => $validators,
+    ];
 
     /*
      * Login Modal Form.
      */
     $form['login_modal'] = [
       '#type' => 'fieldset',
-      '#title' => t('Login Modal Form'),
+      '#title' => $this->t('Login Modal Form'),
       '#description' => $this->t("Variations for displaying the Login Modal Form content."),
     ];
     $form['login_modal']['user_login_header'] = [
@@ -157,12 +221,50 @@ class ConfigForm extends ConfigFormBase {
     $config->set('governor_full_name', $form_state->getValue('governor_full_name'));
     $config->set('governor_last_name', $form_state->getValue('governor_last_name'));
     $config->set('nys_access_permissions_prev_query_ttl', $form_state->getValue('nys_access_permissions_prev_query_ttl'));
+
     $config->set('nys_session_status', $form_state->getValue('nys_session_status'));
     $config->set('nys_session_year', $form_state->getValue('nys_session_year'));
+    $config->set('nys_senate_status', $form_state->getValue('nys_senate_status'));
+    $config->set('nys_vid_redir_ip', $form_state->getValue('nys_vid_redir_ip'));
+    $config->set('nys_vid_redir_ip_2', $form_state->getValue('nys_vid_redir_ip_2'));
+    $config->set('nys_vid_redir_url', $form_state->getValue('nys_vid_redir_url'));
+
+    if ($form_state->getValue('member_listing_pdf')) {
+      $image = $form_state->getValue('member_listing_pdf');
+      $file = File::load($image[0]);
+      if (!empty($file)) {
+        $file->setPermanent();
+        $file->save();
+      }
+      $config->set('member_listing_pdf', $form_state->getValue('member_listing_pdf'));
+    }
+    if ($form_state->getValue('session_calendar_pdf')) {
+      $image = $form_state->getValue('session_calendar_pdf');
+      $file = File::load($image[0]);
+      if (!empty($file)) {
+        $file->setPermanent();
+        $file->save();
+      }
+      $config->set('session_calendar_pdf', $form_state->getValue('session_calendar_pdf'));
+    }
+    if ($form_state->getValue('public_hearing_schedule')) {
+      $image = $form_state->getValue('public_hearing_schedule');
+      $file = File::load($image[0]);
+      if (!empty($file)) {
+        $file->setPermanent();
+        $file->save();
+      }
+      $config->set('public_hearing_schedule', $form_state->getValue('public_hearing_schedule'));
+    }
+
     $config->set('user_login_header', $form_state->getValue('user_login_header'));
     $config->set('user_login_body', $form_state->getValue('user_login_body')['value']);
     $config->set('user_login_footer', $form_state->getValue('user_login_footer')['value']);
     $config->save();
+
+    // Clear the cache.
+    \Drupal::service('cache_tags.invalidator')->invalidateTags(['views:homepage_hero']);
+
     parent::submitForm($form, $form_state);
   }
 

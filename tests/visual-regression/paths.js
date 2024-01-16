be593efc4e053bb53d5003ca5d2883f71dc668e2
Joel Steidl <joel@atendesigngroup.com>
2024-01-16 14:15:03 -0700

    NYS-20: Removes original backstop.js test ad adds configuration and README for Percy tests

--- tests/visual-regression/paths.js
+++
@@ -1,9 +0,0 @@
-var pathConfig = {};
-
-pathConfig.array = [
-  '/', // homepage
-  '/themes/contrib/rain_theme/dist/style-guide/section-components.html', //styleguide components
-  // include other relative URL paths in this array
-]
-
-module.exports = pathConfig;

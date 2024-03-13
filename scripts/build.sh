2985f9c69a946733ce95bdbf00b8a192716c19f1
Joel Steidl <joel@atendesigngroup.com>
2024-03-13 10:05:39 -0600

    Revert "NYS-16: removes uneeded files that were not being used or not relevant to the project"
    This reverts commit 52e53cbe670228f686ea931247cb9ce76c6a507d.

---
+++ scripts/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd $(dirname "$0") && pwd -P)
+BASE_DIR=$(cd ${SCRIPT_DIR}/.. && pwd -P)
+
+cd ${BASE_DIR}
+
+COMPOSER_INSTALLED=$(command -v composer)
+
+if [ -z "${COMPOSER_INSTALLED}" ]; then
+  echo "Composer ( https://getcomposer.org/) not found, please install."
+  exit 1;
+fi
+
+if [ ! -d ${BASE_DIR}/vendor/mediacurrent/ci-scripts ]; then
+  composer install
+fi
+
+CMD=$@
+# Default to run "site:build" if no commands provided.
+if [ -z "${CMD}" ]; then
+  CMD="site:build -Dexisting_config"
+fi
+
+${BASE_DIR}/scripts/hobson ${CMD}

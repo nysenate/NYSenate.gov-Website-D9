1b30419e979dc944a792ed07a5fefca3f7158264
Nathaniel Woodland <nathaniel@atendesigngroup.com>
2024-05-02 10:48:36 -0400

    nys-91: add secondary link to my dashboard actionbar block plugin

--- web/modules/custom/nys_dashboard/src/Plugin/Block/DashboardActionbarBlock.php
+++ web/modules/custom/nys_dashboard/src/Plugin/Block/DashboardActionbarBlock.php
@@ -31,31 +31,8 @@ class DashboardActionbarBlock extends BlockBase {
       '#title' => $this->t('Subheading'),
       '#default_value' => $this->configuration['subheading'] ?? NULL,
     ];
-    $form['featured_link'] = [
-      '#type' => 'fieldset',
-      '#title' => $this->t('Featured link'),
-      'text' => [
-        '#type' => 'textfield',
-        '#title' => $this->t('Link text'),
-        '#default_value' => $this->configuration['featured_link']['text'] ?? NULL,
-      ],
-      'url' => [
-        '#type' => 'textfield',
-        '#title' => $this->t('URL'),
-        '#default_value' => $this->configuration['featured_link']['url'] ?? NULL,
-      ],
-      'icon' => [
-        '#type' => 'textfield',
-        '#title' => $this->t('Icon'),
-        '#default_value' => $this->configuration['featured_link']['icon'] ?? NULL,
-        '#description' => <<<DESC
-            Icons are from the Phosphor icon library. Use the machine name of
-            an icon in this field, e.g. "newspaper-clipping". See
-            <a href="https://phosphoricons.com" target="_blank">
-            https://phosphoricons.com</a>.
-            DESC,
-      ],
-    ];
+    $form['featured_link_1'] = $this->buildFeaturedLinkField(1);
+    $form['featured_link_2'] = $this->buildFeaturedLinkField(2);
     return $form;
   }
 
@@ -66,7 +43,8 @@ class DashboardActionbarBlock extends BlockBase {
     $values = $form_state->getValues();
     $this->configuration['heading'] = $values['heading'];
     $this->configuration['subheading'] = $values['subheading'];
-    $this->configuration['featured_link'] = $values['featured_link'];
+    $this->configuration['featured_link_1'] = $values['featured_link_1'];
+    $this->configuration['featured_link_2'] = $values['featured_link_2'];
   }
 
   /**
@@ -76,14 +54,27 @@ class DashboardActionbarBlock extends BlockBase {
     $heading = $this->configuration['heading'];
     $subheading = $this->configuration['subheading'];
 
-    $featured_link = '';
-    $featured_link_config = $this->configuration['featured_link'];
-    if (!empty($featured_link_config['text'])) {
-      $featured_link = '<a class="dashboard-action-bar-link" href="'
-        . $featured_link_config['url'] . '"><i class="ph ph-'
-        . $featured_link_config['icon'] . '"></i>'
-        . $featured_link_config['text'] . '</a>';
+    $featured_links = [
+      1 => $this->configuration['featured_link_1'],
+      2 => $this->configuration['featured_link_2'],
+    ];
+    $featured_link_1 = '';
+    $featured_link_2 = '';
+    foreach ($featured_links as $link_num => $featured_link) {
+      if (!empty($featured_link['text'])) {
+        ${'featured_link_' . $link_num} =
+          '<a class="dashboard-action-bar-link" href="'
+          . $featured_link['url'] . '"><i class="ph ph-'
+          . $featured_link['icon'] . '"></i>'
+          . $featured_link['text'] . '</a>';
+      }
     }
+    $featured_link_wrapper = <<<HTML
+      <div class='dashboard-action-bar-link-wrapper'>
+        $featured_link_1
+        $featured_link_2
+      </div>
+      HTML;
 
     return [
       '#markup' => <<<END
@@ -91,8 +82,45 @@ class DashboardActionbarBlock extends BlockBase {
         <p class="dashboard-action-bar-subheading">
           <em>$subheading</em>
         </p>
-        $featured_link
+        $featured_link_wrapper
         END,
+    ];
+  }
+
+  /**
+   * Builds the featured link field.
+   *
+   * @param int $link_num
+   *   The link number.
+   *
+   * @return array
+   *   The featured link field.
+   */
+  public function buildFeaturedLinkField(int $link_num): array {
+    return [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Featured link @link_num', ['@link_num' => $link_num]),
+      'text' => [
+        '#type' => 'textfield',
+        '#title' => $this->t('Link text'),
+        '#default_value' => $this->configuration['featured_link_' . $link_num]['text'] ?? NULL,
+      ],
+      'url' => [
+        '#type' => 'textfield',
+        '#title' => $this->t('URL'),
+        '#default_value' => $this->configuration['featured_link_' . $link_num]['url'] ?? NULL,
+      ],
+      'icon' => [
+        '#type' => 'textfield',
+        '#title' => $this->t('Icon'),
+        '#default_value' => $this->configuration['featured_link_' . $link_num]['icon'] ?? NULL,
+        '#description' => <<<DESC
+            Icons are from the Phosphor icon library. Use the machine name of
+            an icon in this field, e.g. "newspaper-clipping". See
+            <a href="https://phosphoricons.com" target="_blank">
+            https://phosphoricons.com</a>.
+            DESC,
+      ],
     ];
   }
 

9d9e57adb8284d6f9fff787167ba1438a4900f33
Nathaniel Woodland <nathaniel@atendesigngroup.com>
2024-05-23 14:05:59 -0400

    nys-95: disable manage dashboard form submit button unless the user makes changes to their preferences

--- web/themes/custom/nysenate_theme/src/patterns/pages/manage-dashboard/manage-dashboard.js
+++ web/themes/custom/nysenate_theme/src/patterns/pages/manage-dashboard/manage-dashboard.js
@@ -4,14 +4,29 @@
  * Behaviors for the Manage Dashboard form.
  */
 ((Drupal, once) => {
-  Drupal.behaviors.nysManageDashboardUncheckAll = {
+  Drupal.behaviors.nysManageDashboard = {
     attach: function () {
+      // Disable submit button by default
+      let submitButton = document.querySelector('#nys-dashboard-manage-dashboard #edit-submit');
+      submitButton.disabled = true;
+
+      // Enable submit button if any input unchecked.
+      let fields = document.querySelectorAll('#nys-dashboard-manage-dashboard input.form-checkbox');
+      fields = Array.from(fields);
+      fields.forEach(field => {
+        field.addEventListener('change', () => {
+          submitButton.disabled = fields.every(field => field.checked); //field.checked;
+        })
+      })
+
+      // Provides "uncheck all" functionality.
       let uncheckAllButtons = document.getElementsByClassName('uncheck-all-button');
       for (let uncheckAllButton of uncheckAllButtons) {
         uncheckAllButton.onclick = function () {
           let checkboxes = uncheckAllButton.closest('.form-checkboxes').getElementsByClassName('form-checkbox');
           for (let checkbox of checkboxes) {
             checkbox.checked = false;
+            submitButton.disabled = false;
           }
           return false;
         };

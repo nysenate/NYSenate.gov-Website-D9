607a8fe6298896b85139fc6309b1f33c9f81cf26
Nathaniel Woodland <nathaniel@atendesigngroup.com>
2024-08-01 14:40:13 -0400

    nys-16449: implement TermMergeSubscriber::onTermMerge

--- web/modules/custom/nys_issues/src/EventSubscriber/TermMergeSubscriber.php
+++ web/modules/custom/nys_issues/src/EventSubscriber/TermMergeSubscriber.php
@@ -11,7 +11,7 @@ use Drupal\term_merge\TermsMergedEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
- * Event subscriber for issue taxonomy events.
+ * Event subscriber for term merge events on the 'issues' vocabulary.
  */
 final class TermMergeSubscriber implements EventSubscriberInterface {
 
@@ -24,10 +24,48 @@ final class TermMergeSubscriber implements EventSubscriberInterface {
   ) {}
 
   /**
-   * Event handler to ensure flags on merged terms are properly rewired.
+   * Recreate flags on to-delete branch terms pointing to preserved trunk terms.
    */
   public function onTermMerge(TermsMergedEvent $event): void {
-    // @todo implement.
+    $flaggable_vid = 'issues';
+    if ($event->getTargetTerm()->bundle() == $flaggable_vid) {
+      $flag_id = 'follow_issue';
+      $merged_tids = array_keys($event->getSourceTerms());
+      $target_tid = $event->getTargetTerm()->id();
+      $flag_storage = $this->entityTypeManager
+        ->getStorage('flagging');
+      $flagging_ids = $flag_storage->getQuery()
+        ->accessCheck(FALSE)
+        ->condition('flag_id', $flag_id)
+        ->condition('entity_id', $merged_tids, 'IN')
+        ->execute();
+
+      /** @var \Drupal\flag\Entity\Flagging[] $flaggings_on_branch_terms */
+      $flaggings_on_branch_terms = $flag_storage->loadMultiple($flagging_ids);
+      foreach ($flaggings_on_branch_terms as $flagging) {
+        // Skip if user has already flagged trunk term.
+        $flagging_uid = $flagging->uid->target_id;
+        $user_has_flag_on_trunk = $flag_storage->getQuery()
+          ->accessCheck(FALSE)
+          ->condition('flag_id', $flag_id)
+          ->condition('entity_id', $target_tid)
+          ->condition('uid', $flagging_uid)
+          ->execute();
+        if (!empty($user_has_flag_on_trunk)) {
+          continue;
+        }
+
+        // Otherwise, recreate flag for user on trunk term.
+        $flag_interface = $this->flag->getFlagById($flag_id);
+        $target_term = $this->entityTypeManager
+          ->getStorage('taxonomy_term')
+          ->load($target_tid);
+        $flagging_user = $this->entityTypeManager
+          ->getStorage('user')
+          ->load($flagging_uid);
+        $this->flag->flag($flag_interface, $target_term, $flagging_user);
+      }
+    }
   }
 
   /**

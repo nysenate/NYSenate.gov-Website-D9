10355a1d133e2cf61e7b369423b0ff32a6298b23
Nathaniel Woodland <nathaniel@atendesigngroup.com>
2024-08-28 15:39:42 -0400

    NYS-168: Update custom handler to restrict edit access to content blocks linked to managed senators

--- web/modules/custom/nys_senators/src/Access/McpBlockContentAccessControlHandler.php
+++ web/modules/custom/nys_senators/src/Access/McpBlockContentAccessControlHandler.php
@@ -17,11 +17,22 @@ class McpBlockContentAccessControlHandler extends BlockContentAccessControlHandl
    * {@inheritdoc}
    */
   protected function checkAccess(EntityInterface $entity, $operation, AccountInterface $account) {
+    // If user is an MCP, grant access to content blocks tied to their senator.
     $current_user = UsersHelper::resolveUser();
     if (UsersHelper::isMcp($current_user)) {
-      return AccessResult::allowed();
+      $managed_senator_tids = UsersHelper::getManagedSenators($current_user);
+      $is_block_linked_to_managed_senator = \Drupal::entityTypeManager()->getStorage('node')->getQuery()
+        ->accessCheck(FALSE)
+        ->condition('field_block', $entity->id(), 'CONTAINS')
+        ->condition('field_senator_multiref', $managed_senator_tids, 'IN')
+        ->count()
+        ->execute();
+      if ($is_block_linked_to_managed_senator) {
+        return AccessResult::allowed();
+      }
     }
 
+    // Otherwise, fall back on core access rules.
     return parent::checkAccess($entity, $operation, $account);
   }
 
@@ -29,11 +40,13 @@ class McpBlockContentAccessControlHandler extends BlockContentAccessControlHandl
    * {@inheritdoc}
    */
   protected function checkCreateAccess(AccountInterface $account, array $context, $entity_bundle = NULL) {
+    // If user is an MCP, grant access to creating new content blocks.
     $current_user = UsersHelper::resolveUser();
     if (UsersHelper::isMcp($current_user)) {
       return AccessResult::allowed();
     }
 
+    // Otherwise, fall back on core access rules.
     return parent::checkCreateAccess($account, $context, $entity_bundle);
   }
 

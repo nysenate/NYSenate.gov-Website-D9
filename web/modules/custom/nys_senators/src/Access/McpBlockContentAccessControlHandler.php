d88509a022a1ad27233f8af8519d47803f9bc08a
Nathaniel Woodland <nathaniel@atendesigngroup.com>
2024-09-10 15:06:20 -0400

    NYS-168: Improve PHP coding standards

--- web/modules/custom/nys_senators/src/Access/McpBlockContentAccessControlHandler.php
+++ web/modules/custom/nys_senators/src/Access/McpBlockContentAccessControlHandler.php
@@ -62,16 +62,23 @@ class McpBlockContentAccessControlHandler extends BlockContentAccessControlHandl
     $current_user = UsersHelper::resolveUser();
     if (UsersHelper::isMcp($current_user)) {
       $managed_senator_tids = UsersHelper::getManagedSenators($current_user);
-      $is_block_linked_to_managed_senator = $this->entityTypeManager
-        ->getStorage('node')
-        ->getQuery()
-        ->accessCheck(FALSE)
-        ->condition('field_block', $entity->id(), 'CONTAINS')
-        ->condition('field_senator_multiref', $managed_senator_tids, 'IN')
-        ->count()
-        ->execute();
-      if ($is_block_linked_to_managed_senator) {
-        return AccessResult::allowed();
+      try {
+        $node_storage = $this->entityTypeManager
+          ->getStorage('node');
+      }
+      catch (\Exception) {
+      }
+      if (isset($node_storage)) {
+        $is_block_linked_to_managed_senator = $node_storage
+          ->getQuery()
+          ->accessCheck(FALSE)
+          ->condition('field_block', $entity->id(), 'CONTAINS')
+          ->condition('field_senator_multiref', $managed_senator_tids, 'IN')
+          ->count()
+          ->execute();
+        if ($is_block_linked_to_managed_senator) {
+          return AccessResult::allowed();
+        }
       }
     }
 

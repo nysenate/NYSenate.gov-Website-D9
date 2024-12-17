caadcd721fc20bcb24cbd6df12f90cbe6793a458
Nathaniel Woodland <nathaniel@atendesigngroup.com>
2024-12-17 17:11:15 -0500

    NYS-83: Add image orientation confirmation pop-up to student submissions form

---
+++ web/modules/custom/nys_school_forms/js/submission-confirmation-popup.js
@@ -0,0 +1,40 @@
+(function (Drupal) {
+  Drupal.behaviors.nysSchoolFormsConfirmationPopup = {
+    attach: function (context) {
+      const form = document.querySelector('form.node-student-submission-container-form');
+      form.addEventListener('submit', (e) => {
+        if (form.dataset.confirmationHandled === 'true') {
+          return;
+        }
+        e.preventDefault();
+        if (!document.getElementById('submission-confirmation-modal')) {
+          const modal = document.createElement('div');
+          modal.id = 'submission-confirmation-modal';
+          modal.className = 'submission-modal-overlay';
+          modal.innerHTML = `
+            <div class="submission-modal">
+              <h2>Wait!</h2>
+              <p>Have you double-checked that all uploaded images have been rotated to their correct orientations? (There is a rotate button next to each uploaded image.)</p>
+              <div class="modal-buttons">
+                <button id="confirm-no" class="modal-button">No, go back</button>
+                <button id="confirm-yes" class="modal-button">Yes, I've confirmed that all image orientations are correct</button>
+              </div>
+            </div>
+          `;
+          document.body.appendChild(modal);
+          document.getElementById('confirm-yes').addEventListener('click', () => {
+            modal.remove();
+            const submitButton = form.querySelector('div#edit-actions input.js-form-submit');
+            if (submitButton) {
+              form.dataset.confirmationHandled = 'true';
+              submitButton.click();
+            }
+          });
+          document.getElementById('confirm-no').addEventListener('click', () => {
+            modal.remove();
+          });
+        }
+      });
+    },
+  };
+})(Drupal);

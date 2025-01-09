fc9b45af7696866410e371836a6131ef4fdcf14e
Nathaniel Woodland <nathaniel@atendesigngroup.com>
2025-01-09 15:23:49 -0500

    NYS-83: Address PR comments and 2nd demo feedback [WIP]

--- web/modules/custom/nys_school_forms/js/submission-confirmation-popup.js
+++ web/modules/custom/nys_school_forms/js/submission-confirmation-popup.js
@@ -1,42 +1,37 @@
-(function (Drupal) {
+(function (Drupal, $) {
   Drupal.behaviors.nysSchoolFormsConfirmationPopup = {
     attach: function (context) {
       const form = document.querySelector('form.node-student-submission-container-form');
+      const form_submit = form.querySelector('div#edit-actions input.js-form-submit');
+      const confirmation_modal = document.getElementById('submission-confirmation-modal');
+      if (!form || !form_submit || !confirmation_modal) {
+        return;
+      }
       form.addEventListener('submit', (e) => {
         if (form.dataset.confirmationHandled === 'true') {
           return;
         }
         e.preventDefault();
-        if (!document.getElementById('submission-confirmation-modal')) {
-          const modal = document.createElement('div');
-          modal.id = 'submission-confirmation-modal';
-          modal.className = 'submission-modal-overlay';
-          modal.innerHTML = `
-            <div class="submission-modal">
-              <h2>Wait!</h2>
-              <p>Image orientations can change after upload.</p>
-              <p><strong>Have you double-checked that all uploaded images have been rotated to their proper orientations?</strong></p>
-              <p>(There is a rotate button next to each uploaded image.)</p>
-              <div class="modal-buttons">
-                <button id="confirm-no" class="modal-button">No, go back</button>
-                <button id="confirm-yes" class="modal-button">Yes, I've confirmed that all image orientations are correct</button>
-              </div>
-            </div>
-          `;
-          document.body.appendChild(modal);
-          document.getElementById('confirm-yes').addEventListener('click', () => {
-            modal.remove();
-            const submitButton = form.querySelector('div#edit-actions input.js-form-submit');
-            if (submitButton) {
+        $('#submission-confirmation-modal').dialog({
+          width: 640,
+          modal: true,
+          buttons: {
+            "No, go back": function () {
+              $(this).dialog("destroy");
+              delete form.dataset.confirmationHandled;
+            },
+            "Yes, I've confirmed that all image orientations are correct": function () {
+              $(this).dialog("destroy");
               form.dataset.confirmationHandled = 'true';
-              submitButton.click();
+              form_submit.click();
             }
-          });
-          document.getElementById('confirm-no').addEventListener('click', () => {
-            modal.remove();
-          });
-        }
+          },
+          close: function () {
+            $(this).dialog("destroy");
+            delete form.dataset.confirmationHandled;
+          }
+        });
       });
     },
   };
-})(Drupal);
+})(Drupal, jQuery);

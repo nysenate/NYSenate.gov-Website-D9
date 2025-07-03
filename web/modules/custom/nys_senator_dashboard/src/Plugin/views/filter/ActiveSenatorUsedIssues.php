5aad746bd66ac03225696f6f648e8b356eec179b
Nathaniel Woodland <nathaniel@atendesigngroup.com>
2025-07-03 15:42:09 -0400

    NYS-76: Misc cleanup

--- web/modules/custom/nys_senator_dashboard/src/Plugin/views/filter/ActiveSenatorUsedIssues.php
+++ web/modules/custom/nys_senator_dashboard/src/Plugin/views/filter/ActiveSenatorUsedIssues.php
@@ -2,21 +2,60 @@
 
 namespace Drupal\nys_senator_dashboard\Plugin\views\filter;
 
-use Drupal\views\Plugin\views\filter\FilterPluginBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\nys_senator_dashboard\Service\ManagedSenatorsHandler;
+use Drupal\views\Plugin\views\filter\FilterPluginBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Exposed filter to show "Issue" terms linked to the active senator.
  *
  * @ViewsFilter("nys_senator_dashboard_active_senator_used_issues")
  */
-class ActiveSenatorUsedIssues extends FilterPluginBase {
+class ActiveSenatorUsedIssues extends FilterPluginBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The Managed Senators Handler service.
+   *
+   * @var \Drupal\nys_senator_dashboard\Service\ManagedSenatorsHandler
+   */
+  protected $managedSenatorsHandler;
+
+  /**
+   * Constructs the ActiveSenatorUsedIssues plugin.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\nys_senator_dashboard\Service\ManagedSenatorsHandler $managedSenatorsHandler
+   *   The managed senators handler service.
+   */
+  public function __construct($configuration, $plugin_id, $plugin_definition, ManagedSenatorsHandler $managedSenatorsHandler) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->managedSenatorsHandler = $managedSenatorsHandler;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('nys_senator_dashboard.managed_senators_handler')
+    );
+  }
 
   /**
    * {@inheritdoc}
    */
   public function buildExposedForm(&$form, FormStateInterface $form_state) {
-    $active_senator = \Drupal::service('nys_senator_dashboard.managed_senators_handler')->ensureAndGetActiveSenator(FALSE);
+    $active_senator = $this->managedSenatorsHandler->ensureAndGetActiveSenator(FALSE);
     $form[$this->options['expose']['identifier']] = [
       '#type' => 'select',
       '#title' => $this->t('Filter By'),
@@ -34,7 +73,7 @@ class ActiveSenatorUsedIssues extends FilterPluginBase {
   public function query() {
     if (!empty($this->value)) {
       $value = reset($this->value);
-      $active_senator_id = \Drupal::service('nys_senator_dashboard.managed_senators_handler')->ensureAndGetActiveSenator();
+      $active_senator_id = $this->managedSenatorsHandler->ensureAndGetActiveSenator();
       // These should always match.
       if ($value == $active_senator_id) {
         $this->query->addWhereExpression(

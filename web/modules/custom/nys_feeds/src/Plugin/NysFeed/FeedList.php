d149e736c5c3567b08b1d917758f8d75fcf19360
Steve Binkowski <binkowsk@nysenate.gov>
2026-07-09 12:41:43 -0400

    Add one feed to rule them all

---
+++ web/modules/custom/nys_feeds/src/Plugin/NysFeed/FeedList.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Drupal\nys_feeds\Plugin\NysFeed;
+
+use Drupal\nys_feeds\FeedState;
+use Drupal\nys_feeds\NysFeedPluginBase;
+use Drupal\nys_feeds\Service\NysFeedPluginManager;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * NYS Feeds plugin to list all available feeds.
+ *
+ * @NysFeed(
+ *   id = "feed_list",
+ *   label = @Translation("Feed List"),
+ *   description = @Translation("Returns all known feeds"),
+ * )
+ */
+class FeedList extends NysFeedPluginBase {
+
+  /**
+   * The NYS Feed Plugin Manager service.
+   *
+   * @var \Drupal\nys_feeds\Service\NysFeedPluginManager
+   */
+  private NysFeedPluginManager $feedManager;
+
+  /**
+   * Constructor.
+   */
+  public function __construct(NysFeedPluginManager $feedManager, array $definition, array $config = []) {
+    parent::__construct($definition, $config);
+    $this->feedManager = $feedManager;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): static {
+    return new static($container->get('nys_feeds.feed_manager'), $plugin_definition, $configuration);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function getFeed(FeedState $state): FeedState {
+    $state->data = [];
+    foreach ($this->feedManager->getDefinitions() as $definition) {
+      $state->data[$definition['id']] = $definition['label'];
+    }
+    return $state;
+  }
+
+}
